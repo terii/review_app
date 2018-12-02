f967771ec16093569005af1a6ceb4d05954e7b08
terii <e175726@ie.u-ryukyu.ac.jp>
2018-12-02 17:46:06 +0900

    init

---
+++ app_review/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app_review
+//
+//  Created by hidaka seiryu on 2018/11/29.
+//  Copyright © 2018 hidaka seiryu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
